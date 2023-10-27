841d020d60437598652b8d7a53adf76e89a8b4e1
hadhi hassan m <136085916+hadhihasssn@users.noreply.github.com>
2023-10-26 22:06:24 -0700

    Time: 60 ms (73.1%), Space: 44.3 MB (74.92%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+let i=0
+
+
+for(let j=0;j<nums.length;j++){
+  if(nums[j]!=nums[i]){
+    nums[++i] = nums[j]
+  }
+}
+  return ++i
+
+
+}; 

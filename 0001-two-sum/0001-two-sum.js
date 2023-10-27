98bbfcae74b44bdd9516876354064d529d90f991
hadhi hassan m <136085916+hadhihasssn@users.noreply.github.com>
2023-10-26 22:04:35 -0700

    Time: 76 ms (49.03%), Space: 41.8 MB (94.94%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(var i=0;i<nums.length;i++){
+        for(var j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j] === target){
+                return [i,j]
+            }
+        }
+    }
+    
+};
+console.log(twoSum([1,2,3],4))
